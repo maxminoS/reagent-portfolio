2b85f9573b53bd65c5bbab813b88274b589c1efa
maxminoS <maxminos.1@gmail.com>
2021-06-26 22:09:34 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Reagent Portfolio
+
+A portfolio created using ClojureScript with its React wrapper: Reagent
+
+This project uses Shadow CLJS as its build tool, Reitit for routing, Garden for styling, and may later be reworked to use Re-frame.
+
+## Development
+`yarn` - Install dependencies
+
+`yarn dev` - Start development
+
+`yarn release` - Build for production
+
+`yarn server` - Serve
---
+++ src/reagent_portfolio/core.cljs
@@ -0,0 +1,56 @@
+(ns reagent-portfolio.core
+  (:require
+   [reagent-portfolio.state :as state]
+   [reagent.dom :as d]))
+
+(defn nav-bar []
+  [:ul
+   [:li [:a {:href "#home"} "Home"]]
+   [:li [:a {:href "#blog" :style {:pointer-events "none" :color "red"}} "Blog"]] ; disabled
+   [:li [:a {:href "#projects"} "Projects"]]
+   [:li [:a {:href "#contact"} "Contact"]]])
+
+(defn tag-block [tag]
+  [:h6.tag tag])
+
+(defn showcase-card [{:keys [name abstract image tags link source]}]
+  [:li.card
+   [:img {:src image
+          :alt name}]
+   [:h3 name]
+   [:h5 abstract]
+   (for [tag tags]
+     ^{:key tag} [tag-block tag])
+   [:a {:href link} "Live"]
+   [:a {:href source} "Source"]])
+
+(defn showcase []
+  [:div
+   [:h2 "Projects"]
+   (for [{:keys [id name abstract image tags link source]} (vals @state/showcase)]
+     [showcase-card {:key id
+                     :name name
+                     :abstract abstract
+                     :image image
+                     :tags tags
+                     :link link
+                     :source source}])
+   [:button "View more"]])
+
+(defn home-page []
+  [:div
+   [:h2 "Welcome"]
+   [nav-bar]
+   [showcase]])
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
+
+ ;; Home || Blog || Projects || Contact || Emacs(logo)
+ ;; Showcase: https://mattfarley.ca/ (view more)
+ ;; Projects: github/repos_list
+
+ ;; README: https://github.com/rcaferati/rcaferati
