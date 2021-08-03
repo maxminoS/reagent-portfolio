703561edb182f02937b162f0b9261a499634c768
maxminoS <maxminos.1@gmail.com>
2021-08-03 17:21:40 +0700

    Update states

--- src/reagent_portfolio/state.cljs
+++ src/reagent_portfolio/state.cljs
@@ -5,54 +5,42 @@
 (def showcase (r/atom {:showcase-1 {:id :showcase-1
                                     :name "Shipment"
                                     :abstract "A file hosting web application"
-                                    :description "A simple, responsive file hosting web application, returning a shortened URL to download the uploaded file"
                                     :image "static/shipment-card.png"
-                                    :category ["Web"]
                                     :tags ["React" "TypeScript" "Tailwind CSS" "Firebase"]
                                     :link "https://decompiler-shipment.web.app"
                                     :source "https://github.com/The-Decompiler/shipment"}
                        :showcase-2 {:id :showcase-2
                                     :name "Ultraviolet"
                                     :abstract "Remotely control your computer with a mobile app"
-                                    :description "Send touchpad signals and keyboard presses remotely through a TCP connection between your phone and your computer"
                                     :image "static/ultraviolet-card.png"
-                                    :category ["Mobile"]
-                                    :tags ["React Native" "TypeScript" "Go" "Sockets"]
+                                    :tags ["React Native" "TypeScript" "Go" "Network"]
                                     :link ""
                                     :source "https://github.com/The-Decompiler/ultraviolet"}
                        :showcase-3 {:id :showcase-3
                                     :name "Neurage"
                                     :abstract "Take a picture and see what age you look like"
-                                    :description "A web application that will predict your age based on the image you take"
                                     :image "static/neurage-card.png"
-                                    :category ["Web" "Machine Learning"]
                                     :tags ["React" "TypeScript" "Styled Components" "Python" "FastAPI"]
                                     :link "https://neurage.netlify.app"
                                     :source "https://github.com/The-Decompiler/neurage"}
                        :showcase-4 {:id :showcase-4
                                     :name "Link Shrink"
                                     :abstract "A simple URL shortener"
-                                    :description "Shortens a total of 25 links to distinct URL links"
                                     :image "static/link-shrink-card.png"
-                                    :category ["Web"]
                                     :tags ["React" "TypeScript" "Node" "MongoDB" "Tailwind CSS" "Formik"]
                                     :link "https://link-shrink.netlify.app/"
                                     :source "https://github.com/maxminoS/link-shrink"}
                        :showcase-5 {:id :showcase-5
                                     :name "Quote Box"
                                     :abstract "Displays random quotes from an API"
-                                    :description "Display a tweetable random quote fetched from an API, complete with a testing suite"
                                     :image "static/quote-box-card.png"
-                                    :category ["Web"]
                                     :tags ["React" "TypeScript" "Jest" "Cypress" "React Testing" "Material UI"]
                                     :link "https://the-quote-box.netlify.app"
                                     :source "https://github.com/maxminoS/quote-box"}
                        :showcase-6 {:id :showcase-6
                                     :name "Pure Searchbar"
                                     :abstract "Searchbar for countries and cities"
-                                    :description ""
                                     :image "static/pure-searchbar-card.png"
-                                    :category ["Web"]
                                     :tags ["HTML" "CSS" "JavaScript"]
                                     :link "https://pure-searchbar.netlify.app"
                                     :source "https://github.com/maxminoS/pure-searchbar"}}))
@@ -75,7 +63,7 @@
                                     :name "Ultraviolet"
                                     :abstract "Remotely control your computer with a mobile app"
                                     :category ["Mobile"]
-                                    :tags ["React Native" "TypeScript" "Go" "Sockets"]
+                                    :tags ["React Native" "TypeScript" "Go" "Network"]
                                     :link ""
                                     :source "https://github.com/The-Decompiler/ultraviolet"}
                        :project-04 {:id :project-04
@@ -101,53 +89,53 @@
                                     :source "https://github.com/maxminoS/quote-box"}
                        :project-07 {:id :project-07
                                     :name "Pure Searchbar"
-                                    :abstract "A responsive search functionality built purely using HTML, CSS, and vanilla JavaScript"
+                                    :abstract "Searchbar for countries and cities"
                                     :category ["Web"]
                                     :tags ["HTML" "CSS" "JavaScript"]
                                     :link "https://pure-searchbar.netlify.app"
                                     :source "https://github.com/maxminoS/pure-searchbar"}
                        :project-08 {:id :project-08
-                                    :name "Markup Parser"
-                                    :abstract "Parses markup text and previews it"
-                                    :category ["Web"]
-                                    :tags ["React" "TypeScript" "Material UI" "Org Mode"]
-                                    :link "https://markup-parser.netlify.app"
-                                    :source "https://github.com/maxminoS/markup-parser"}
-                       :project-09 {:id :project-09
-                                    :name "Pure Microprojects"
-                                    :abstract "A variety of responsive micro web projects built purely using HTML, CSS, and vanilla JavaScript"
-                                    :category ["Web"]
-                                    :tags ["HTML" "CSS" "JavaScript"]
-                                    :link "https://pure-projects.netlify.app"
-                                    :source "https://github.com/maxminoS/pure-js"}
-                       :project-10 {:id :project-10
-                                    :name "Flask Generator"
-                                    :abstract "A script to generate a Flask website template"
-                                    :category ["Web" "Template"]
-                                    :tags ["Python" "Flask" "Bootstrap" "SQLite" "Authentication"]
+                                    :name "Watchalong"
+                                    :abstract "A media synchronization project for mpv"
+                                    :category ["Network"]
+                                    :tags ["Go" "Lua" "mpv"]
                                     :link ""
-                                    :source "https://github.com/maxminoS/flask-gen"}
-                       :project-11 {:id :project-11
+                                    :source "https://github.com/The-Decompiler/watchalong"}
+                       :project-09 {:id :project-09
                                     :name "Fizzboxd"
                                     :abstract "A Leterboxd film feed RSS reader Discord bot"
                                     :category ["Bot"]
                                     :tags ["Go" "SQLite"]
                                     :link ""
                                     :source "https://github.com/The-Decompiler/fizzboxd"}
-                       :project-12 {:id :project-12
+                       :project-10 {:id :project-10
                                     :name "Samaritan"
                                     :abstract "Uploads all attachments in a Discord channel to Google Drive"
                                     :category ["Bot"]
-                                    :tags ["Python" "Google Drive"]
+                                    :tags ["Python" "Google API"]
                                     :link ""
                                     :source "https://github.com/maxminoS/samaritan"}
-                       :project-13 {:id :project-13
-                                    :name "Watchalong"
-                                    :abstract "A media synchronization project for mpv"
-                                    :category ["Sockets"]
-                                    :tags ["Go" "Lua" "mpv"]
+                       :project-11 {:id :project-11
+                                    :name "Pure Microprojects"
+                                    :abstract "A variety of responsive micro web projects built purely using HTML, CSS, and vanilla JavaScript"
+                                    :category ["Web"]
+                                    :tags ["HTML" "CSS" "JavaScript"]
+                                    :link "https://pure-projects.netlify.app"
+                                    :source "https://github.com/maxminoS/pure-js"}
+                       :project-12 {:id :project-12
+                                    :name "Flask Generator"
+                                    :abstract "A script to generate a Flask website template"
+                                    :category ["Web" "Template"]
+                                    :tags ["Python" "Flask" "Bootstrap" "SQLite" "Authentication"]
                                     :link ""
-                                    :source "https://github.com/The-Decompiler/watchalong"}
+                                    :source "https://github.com/maxminoS/flask-gen"}
+                       :project-13 {:id :project-13
+                                    :name "Markup Parser"
+                                    :abstract "Parses markup text and previews it"
+                                    :category ["Web"]
+                                    :tags ["React" "TypeScript" "Material UI" "Org Mode"]
+                                    :link "https://markup-parser.netlify.app"
+                                    :source "https://github.com/maxminoS/markup-parser"}
                        :project-14 {:id :project-14
                                     :name "Oolong"
                                     :abstract "A simple shell script to rename all the files in the directory to include their lengths"
@@ -158,7 +146,7 @@
                        :project-15 {:id :project-15
                                     :name "HTTP Server"
                                     :abstract "A simple HTTP server from scratch"
-                                    :category ["Sockets"]
+                                    :category ["Network"]
                                     :tags ["Python" "HTTP"]
                                     :link ""
                                     :source "https://github.com/maxminoS/http-server"}
